3b6608189bf01e5b4b4cef13ea9cc04f343d0884
Aaron Martins <me@aaronmartins.com>
2020-05-03 17:15:07 -0700

    Super awesome install

--- install-on-pi.sh
+++ install-on-pi.sh
@@ -1,9 +1,15 @@
-#!/bin/sh
+#!/bin/sh -x
 
-# Run as sudo on pi
+# Install crontab
+mkdir -p ./tmp
+crontab -l > ./tmp/original-crons
+cp ./tmp/original-crons ./tmp/new-crons
+cat ./install/cron/martins-sprinklers >> ./tmp/new-crons
+crontab ./tmp/new-crons
 
-sudo cp install/cron/martins-sprinklers /etc/cron.d/
-
+# Set up default logfile
 sudo touch /var/log/martins-sprinklers.log
+sudo chown pi:pi /var/log/martins-sprinklers.log
 
+# Install npm dependencies. Note NO sudo here
 npm install --production
