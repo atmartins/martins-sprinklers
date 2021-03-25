ae4989624fc021eab2e9f2ec82e20fe8ac1ba96a
Aaron Martins <me@aaronmartins.com>
2021-03-25 10:26:19 -0700

    Fixes for client

--- client/src/api.tsx
+++ client/src/api.tsx
@@ -1,8 +1,4 @@
 import { API_LOCATION } from "./constants";
 import { ZoneInfo } from "./zoneInfo";
 
-export const persistZoneState = async (zoneInfo: ZoneInfo):Promise<ZoneInfo> => {
-    console.log('Would have called', zoneInfo, `fetch(${API_LOCATION}/channel/${zoneInfo.id}/${zoneInfo.state}).then(rsp => rsp.json());`);
-    // return Promise.resolve(zoneInfo)
-    return fetch(`${API_LOCATION}/channel/${zoneInfo.id}/${zoneInfo.state}`).then(rsp => rsp.json());
-}
+export const persistZoneState = async (zoneInfo: ZoneInfo):Promise<ZoneInfo> => fetch(`${API_LOCATION}/channel/${zoneInfo.id}/${zoneInfo.state}`).then(rsp => rsp.json());
